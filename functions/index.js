463bf8f2e7049ebacfd55bc7b3252096e81278df
asmallgreendot <randompenguingaming@gmail.com>
2020-05-06 20:32:28 +1000

    setup cumshaft

---
+++ functions/index.js
@@ -0,0 +1,11 @@
+const path = require("path")
+const fs = require("fs")
+const imageDirectory = path.join(__dirname, "../static/images")
+const pageGenerator = img => `<image src="/${img}" />`
+
+module.exports = (req, res) => {
+    fs.readdir(imageDirectory,(e,files)=>{
+        const imageName = files[Math.floor(Math.random()*files.length)]
+        res.send (pageGenerator(imageName))
+    })    
+}
