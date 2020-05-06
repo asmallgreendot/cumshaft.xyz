1f6d287773b3498b50b3fbde832b96c4e7a9749b
asmallgreendot <randompenguingaming@gmail.com>
2020-05-06 20:47:03 +1000

    fix images

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require("path")
 const fs = require("fs")
 const imageDirectory = path.join(__dirname, "../static/images")
-const pageGenerator = img => `<image src="/${img}" />`
+const pageGenerator = img => `<image src="/static/images/${img}" />`
 
 module.exports = (req, res) => {
     fs.readdir(imageDirectory,(e,files)=>{
