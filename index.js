1313c43d6eab5ecd2ad0df083a77b7524a9d8c39
asmallgreendot <randompenguingaming@gmail.com>
2020-05-06 21:02:53 +1000

    final fix pls

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require("path")
 const fs = require("fs")
-const imageDirectory = path.join(__dirname, "../static/images")
+const imageDirectory = path.join(__dirname, "static/images")
 const pageGenerator = img => `<image src="/static/images/${img}" />`
 
 module.exports = (req, res) => {
